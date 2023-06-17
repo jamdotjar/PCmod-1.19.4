d5018a93754477127e00a10ec10b3ec835342e38
jamdotjar <darkestknight3d@outlook.com>
2023-06-17 15:35:51 -0700

    Initial commit

---
+++ src/main/java/com/jamdotjar/pcmod/PcModClient.java
@@ -0,0 +1,11 @@
+package com.jamdotjar.pcmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PcModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/jamdotjar/pcmod/PcModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jamdotjar.pcmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PcModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

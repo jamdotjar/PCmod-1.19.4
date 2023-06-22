fba89f95dacc33c01dd76ee63dbb271ad78ce330
jamdotjar <darkestknight3d@outlook.com>
2023-06-21 18:25:19 -0700

    added CPU GPU SSD and RAM

--- src/main/java/com/jamdotjar/pcmod/PcMod.java
+++ src/main/java/com/jamdotjar/pcmod/PcMod.java
@@ -1,7 +1,13 @@
 package com.jamdotjar.pcmod;
 
+import com.jamdotjar.pcmod.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,12 +15,9 @@ public class PcMod implements ModInitializer {
 	public static final String MOD_ID = "pcmod";
     public static final Logger LOGGER = LoggerFactory.getLogger("pcmod");
 
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/jamdotjar/pcmod/items/ModItems.java
@@ -0,0 +1,46 @@
+package com.jamdotjar.pcmod.items;
+
+import com.jamdotjar.pcmod.PcMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    //item declaration
+    public static final Item GPU = RegisterItem("gpu",
+        new Item(new FabricItemSettings()));
+    public static final Item CPU = RegisterItem("cpu",
+            new Item(new FabricItemSettings()));
+    public static final Item RAM = RegisterItem("ram",
+            new Item(new FabricItemSettings()));
+
+    public static final Item SSD_NVME = RegisterItem("ssd_nvme",
+            new Item(new FabricItemSettings()));
+
+
+    private static Item RegisterItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(PcMod.MOD_ID, name ), item);
+    }
+    public static void addItemsToItemGroup(){
+        addToItemGroup(ItemGroups.INGREDIENTS, GPU);
+        addToItemGroup(ItemGroups.INGREDIENTS, CPU);
+        addToItemGroup(ItemGroups.INGREDIENTS, RAM);
+        addToItemGroup(ItemGroups.INGREDIENTS, SSD_NVME);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item){
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+    public static void registerModItems() {
+        PcMod.LOGGER.info("Registerin the Mod items for"+PcMod.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+
+}
